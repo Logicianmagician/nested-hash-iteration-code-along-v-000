d6611a08b7fbfa49d382e1bd32cd859a88b26c2b
Andrew Kairys <andrewkairys@gmail.com>
2018-03-09 17:17:16 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts["Freddy Mercury"].each do |key, value|
     if key == :favorite_ice_cream_flavors
-      :favorite_ice_cream_flavors.to_s.to_a.shift[0]
+      :favorite_ice_cream_flavors.shift[value]
     end
   end
 end
